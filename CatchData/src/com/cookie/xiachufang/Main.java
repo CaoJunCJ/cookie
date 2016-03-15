0c9beb379a3ccd345be5dff1366bdec945fcb8f1
CaoJunCJ <jim.cao.jc@gmail.com>
2016-03-15 15:23:38 +0800

    add test to main class

--- CatchData/src/com/cookie/xiachufang/Main.java
+++ CatchData/src/com/cookie/xiachufang/Main.java
@@ -1,12 +1,16 @@
 package com.cookie.xiachufang;
 
+import java.io.IOException;
+
 public class Main {
 	
-	
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		XiaChuFangParse x = new XiaChuFangParse();
+		try {
+			x.simpleParse();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
